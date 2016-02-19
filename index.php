4bb68e48bfe09297ddf0f216f2f53f4a053ebcd3
WojtekSasiela <wojtek.sasiela@gmail.com>
2016-02-19 11:46:12 +0100

    dodanie FAQ do strony

--- index.php
+++ index.php
@@ -997,83 +997,88 @@
         <!--</div>-->
 </section>
 
-<!--<section id="faq" class="section faq">-->
-<!--<div class="container">-->
-<!--<div class="row">-->
-<!--<div class="col-md-12">-->
-<!--<h3 class="section-title">Event FAQs</h3>-->
-<!--</div>-->
-<!--</div>-->
-<!--<div class="row">-->
-<!--<div class="col-md-12">-->
-<!--<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">-->
-<!---->
-<!--<div class="panel panel-default">-->
-<!--<div class="panel-heading" role="tab" id="headingOne">-->
-<!--<h4 class="panel-title">-->
-<!--<a class="faq-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne"> What is the price of the ticket ?</a>-->
-<!--</h4>-->
-<!--</div>-->
+<section id="faq" class="section faq">
+<div class="container">
+<div class="row">
+<div class="col-md-12">
+<h3 class="section-title">Najszczęstsze zadawane pytania</h3>
+</div>
+</div>
+<div class="row">
+<div class="col-md-12">
+<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 
-<!--<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">-->
-<!--<div class="panel-body">-->
-<!--<h3>Hello</h3>-->
-<!--<p>Lorem Ipsum</p>-->
-<!--</div>-->
-<!--</div>-->
-<!--</div>-->
+<div class="panel panel-default">
+<div class="panel-heading" role="tab" id="headingOne">
+<h4 class="panel-title">
+<a class="faq-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+    Jak dojechać na miejsce ze stacji PKS?</a>
+</h4>
+</div>
 
-<!--<div class="panel panel-default">-->
-<!--<div class="panel-heading" role="tab" id="headingTwo">-->
-<!--<h4 class="panel-title">-->
-<!--<a class="faq-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo"> What is included in my ticket ?</a>-->
-<!--</h4>-->
-<!--</div>-->
+<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+<div class="panel-body">
+<h3></h3>
+<p>Liniami autobusowymi: 10, 151. </p>
+</div>
+</div>
+</div>
 
-<!--<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">-->
-<!--<div class="panel-body">Hello</div>-->
-<!--</div>-->
-<!--</div>-->
-<!---->
-<!--<div class="panel panel-default">-->
-<!--<div class="panel-heading" role="tab" id="headingThree">-->
-<!--<h4 class="panel-title">-->
-<!--<a class="faq-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree"> Office address ?</a>-->
-<!--</h4>-->
-<!--</div>-->
+<div class="panel panel-default">
+<div class="panel-heading" role="tab" id="headingTwo">
+<h4 class="panel-title">
+<a class="faq-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+    Mam dietę i potrzebuję specialnego posiłku, czy mam to gdzieś zgłosić?</a>
+</h4>
+</div>
 
-<!--<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">-->
-<!--<div class="panel-body">Hello</div>-->
-<!--</div>-->
-<!--</div>-->
+<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+<div class="panel-body">Tak, wystarczy napisać na nasz e-mail o swojej diecie konferencja.ss@gmail.com </div>
+</div>
+</div>
 
-<!--<div class="panel panel-default">-->
-<!--<div class="panel-heading" role="tab" id="headingFour">-->
-<!--<h4 class="panel-title">-->
-<!--<a class="faq-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour"> How should I dress ?</a>-->
-<!--</h4>-->
-<!--</div>-->
+<div class="panel panel-default">
+<div class="panel-heading" role="tab" id="headingThree">
+<h4 class="panel-title">
+<a class="faq-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+    Jakies inne pytania?</a>
+</h4>
+</div>
 
-<!--<div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">-->
-<!--<div class="panel-body">Hello</div>-->
-<!--</div>-->
-<!--</div>-->
+<div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+<div class="panel-body">Z chęcia na nie odpowiemy :) </div>
+</div>
+</div>
 
-<!--<div class="panel panel-default">-->
-<!--<div class="panel-heading" role="tab" id="headingFive">-->
-<!--<h4 class="panel-title">-->
-<!--<a class="faq-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive"> I have specific questions that are not addressed here. Who can help me ?</a>-->
-<!--</h4>-->
-<!--</div>-->
+<div class="panel panel-default">
+<div class="panel-heading" role="tab" id="headingFour">
+<h4 class="panel-title">
+<a class="faq-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+    ????????????</a>
+</h4>
+</div>
 
-<!--<div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">-->
-<!--<div class="panel-body">Hello</div>-->
-<!--</div>-->
-<!--</div>-->
-<!--</div>-->
-<!--</div>-->
-<!--</div>-->
-<!--</section>-->
+<div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+<div class="panel-body">Tutaj jest odpowiedż na powyższe pytanie.</div>
+</div>
+</div>
+
+<div class="panel panel-default">
+<div class="panel-heading" role="tab" id="headingFive">
+<h4 class="panel-title">
+<a class="faq-toggle collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+    ???????????</a>
+</h4>
+</div>
+
+<div id="collapseFive" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFive">
+<div class="panel-body"> Odpowiedz na pytanie </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
 
 <!--<section id="photos" class="section photos">-->
 <!--<div class="container">-->
